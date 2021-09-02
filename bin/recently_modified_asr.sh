3678bb2c8b1a856471c855faf7ed70e0a5fe4a48
Joseph Rudoler <maint@node38.rhino.psych.upenn.edu>
2021-09-02 11:43:41 -0400

    Updated environment name

--- bin/recently_modified_asr.sh
+++ bin/recently_modified_asr.sh
@@ -1,2 +1,2 @@
-source activate deepspeech
-/home1/maint/.conda/envs/deepspeech/bin/python /home1/maint/automatic_annotation/scripts/asr_pipeline.py /home1/maint/automatic_annotation/config/config.ini
+source activate deepspeech2
+/home1/maint/.conda/envs/deepspeech2/bin/python /home1/maint/automatic_annotation/scripts/asr_pipeline.py /home1/maint/automatic_annotation/config/config.ini
---
+++ bin/recently_modified_postprocessing.sh
@@ -0,0 +1,2 @@
+source activate deepspeech2
+/home1/maint/.conda/envs/deepspeech2/bin/python /home1/maint/automatic_annotation/scripts/post_processing_pipeline.py /home1/maint/automatic_annotation/config/config.ini
